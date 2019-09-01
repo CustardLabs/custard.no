1ee3d41885b64642138a79f811e2fa9e78b6e991
Russ Flynn <russ@custard.no>
2019-09-01 15:48:11 +0200

    New job postings

---
+++ content/jobs.php
@@ -0,0 +1,68 @@
+<div class="container">
+  <h2>Jobs</h2>
+  <div class="row">
+    <div class="col-md-12">
+      <h3>Web application developer - full time</h3>
+      <img src="img/PHP-logo.svg" class="inline_avatar_right"/>
+      <p>
+        We are looking for someone to help with the development of our core membership system. Candidates should be able to manage their own time
+        and combine remote work with some office time at our Oslo location. Working language (documentation, tickets, project related discussions) is English, however
+        Norwegian (or Swedish) is also highly desirable for some direct customer communications.
+      </p>
+      <p>
+        Ideally you would be able to lighten the load on the existing team in all areas, so the list of "desired" skills is quite long, however we are
+        of course open to realistic proposals. Most important is to get another (full stack) developer on board, it is not the end of the world if that person 
+        is not interested in customer meetings, sales, office operations, etc.
+      </p>
+      <h4>Core skills/experience</h4>
+      <ul>
+        <li>First generation Symfony & Doctrine - required for development & maintenance of existing system(s)</li>
+        <li>New generation Symfony & Doctrine - for moving forward with new development</li>
+        <li>PHP - Main working programming language</li>
+        <li>Some design/UX (html/css/jQuery)</li>
+        <li>Some database interaction (PostgreSQL)</li>
+      </ul>
+      <h4>Other areas of work / nice-to-have skills/experience</h4>
+      <ul>
+        <li>Ability to talk to customers and discuss new functionality and ongoing issues</li>
+        <li>Wordpress, Drupal, Git(hub)</li>
+        <li>Ability to talk to potential customers, demo the system(s) and "sell" services</li>
+        <li>API (in particular JSON, JWT based)</li>
+      </ul>
+      <h4>About the job</h4>
+      <p>
+        Most of our time is spent working through new feature requests and supporting the operations of organisations using the
+        membership system. Work is mostly carried out remotely, using tools such as Slack to communicate with other team members and customers.
+        We also have an office location in Oslo for team meetups and closer contact with our customer(s). The company head office is based in Fredrikstad,
+        so candidates from the Østfold area may be ideally suited as more frequent team meetings could occur there. We also travel to Stockholm around 3-4 times
+        per year to maintain good contact touch with our customers there.
+      </p>
+      <p>
+        Going forward, we will be upgrading the membership system to make use of later versions of PHP and Symfony/Doctrine. This will also involve
+        a large focus on a modernised UI and full-blooded API.
+      </p>
+      <h4>Renumeration</h4>
+      <p>
+        Base salary will be based on experience and set at an industry-standard level - however as a small company with a stable customer base, the potential for
+        a rewarding profit-share bonus model is extremely good. We also support open source contribution (where relevant, e.g. Symfony/PHP/etc.) and attendance
+        at conferences around Europe. Working hours are extremely flexible, and a good pension scheme will be discussed & arranged.
+      </p>  
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h3>UX design - part time</h3>
+      <img src="img/logo-html-css-javascript.png" class="inline_avatar_left" />
+      <p>
+        If you are interested in contributing on an ad-hoc basis, please get in touch.
+      </p>
+    </div>
+    <div class="col-md-12">
+      <h3>Expert consultation - short-term/part-time</h3>
+      <img src="img/symfony_black_03.png" class="inline_avatar_left" />
+      <p>
+        Particularly experienced in Symfony/Doctrine migrations? Get in touch!
+      </p>
+    </div>
+  </div>
+</div>
